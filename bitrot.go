fe8204a8ef8d2add45015af3d0ee7fd4a27328fe
Gabriel Gilder <gabriel@gabrielgilder.com>
2015-01-18 22:00:14 -0800

    Split out manifest into separate file

--- bitrot.go
+++ bitrot.go
@@ -1,70 +1,12 @@
 package bitrot
 
 import (
-	"crypto/sha1"
-	"encoding/hex"
-	"io/ioutil"
-	"os"
 	"path/filepath"
 	"strings"
-	"time"
 )
 
 type Config struct {
 	ExcludedFiles []string
-}
-
-type ChecksumRecord struct {
-	Checksum string    `json:"checksum"`
-	ModTime  time.Time `json:"mod_time"`
-}
-
-type DirectoryManifest struct {
-	Path      string                    `json:"path"`
-	CreatedAt time.Time                 `json:"created_at"`
-	Entries   map[string]ChecksumRecord `json:"entries"`
-}
-
-type ManifestComparison struct {
-	DeletedPaths  []string
-	AddedPaths    []string
-	ModifiedPaths []string
-	FlaggedPaths  []string
-}
-
-func FileChecksum(file string) ChecksumRecord {
-	fi, err := os.Stat(file)
-	check(err)
-
-	sum := generateChecksum(file)
-	return ChecksumRecord{
-		Checksum: sum,
-		ModTime:  fi.ModTime(),
-	}
-}
-
-func GenerateDirectoryManifest(path string, config *Config) DirectoryManifest {
-	return DirectoryManifest{
-		Path:      path,
-		CreatedAt: time.Now(),
-		Entries:   directoryChecksums(path, config),
-	}
-}
-
-func CompareManifests(oldManifest, newManifest DirectoryManifest) (comparison ManifestComparison) {
-	checkAddedPaths(&oldManifest, &newManifest, &comparison)
-	checkChangedPaths(&oldManifest, &newManifest, &comparison)
-	return comparison
-}
-
-// Private functions
-
-func generateChecksum(file string) string {
-	data, err := ioutil.ReadFile(file)
-	check(err)
-
-	sum := sha1.Sum(data)
-	return hex.EncodeToString(sum[:])
 }
 
 func isIgnoredPath(path string, ignored *[]string) bool {
@@ -77,66 +19,4 @@ func isIgnoredPath(path string, ignored *[]string) bool {
 		}
 	}
 	return false
-}
-
-func directoryChecksums(path string, config *Config) map[string]ChecksumRecord {
-	records := map[string]ChecksumRecord{}
-	filepath.Walk(path, func(entryPath string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if isIgnoredPath(entryPath, &config.ExcludedFiles) {
-			return nil
-		}
-
-		if info.Mode().IsRegular() {
-			var relPath string
-			relPath, err = filepath.Rel(path, entryPath)
-			check(err)
-			records[relPath] = ChecksumRecord{
-				Checksum: generateChecksum(entryPath),
-				ModTime:  info.ModTime(),
-			}
-		}
-
-		return nil
-	})
-	return records
-}
-
-func checkAddedPaths(oldManifest, newManifest *DirectoryManifest, comparison *ManifestComparison) {
-	for path, _ := range newManifest.Entries {
-		_, oldEntryPresent := oldManifest.Entries[path]
-		if !oldEntryPresent {
-			comparison.AddedPaths = append(comparison.AddedPaths, path)
-		}
-	}
-}
-
-func checkChangedPaths(oldManifest, newManifest *DirectoryManifest, comparison *ManifestComparison) {
-	for path, oldEntry := range oldManifest.Entries {
-		newEntry, newEntryPresent := newManifest.Entries[path]
-		if newEntryPresent {
-			checkModifiedPath(path, &oldEntry, &newEntry, comparison)
-		} else {
-			comparison.DeletedPaths = append(comparison.DeletedPaths, path)
-		}
-	}
-}
-
-func checkModifiedPath(path string, oldEntry, newEntry *ChecksumRecord, comparison *ManifestComparison) {
-	if newEntry.Checksum != oldEntry.Checksum {
-		if newEntry.ModTime != oldEntry.ModTime {
-			comparison.ModifiedPaths = append(comparison.ModifiedPaths, path)
-		} else {
-			comparison.FlaggedPaths = append(comparison.FlaggedPaths, path)
-		}
-	}
-}
-
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
 }
---
+++ manifest_test.go
@@ -0,0 +1,231 @@
+package bitrot
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"math"
+	"os"
+	"path/filepath"
+	"reflect"
+	"testing"
+	"time"
+)
+
+var helloWorldString = "hello! world\n"
+var helloWorldChecksum = "87b3fe7479c73ae4246dbe8081550f52e2cf9e59"
+
+func writeTestFile(dir, name, content string) string {
+	testFile := filepath.Join(dir, name)
+	err := ioutil.WriteFile(testFile, []byte(content), 0400)
+	check(err)
+	return testFile
+}
+
+func populateTestDirectory(tempDir string) (map[string]string, time.Time) {
+	writeTestFile(tempDir, "foo", helloWorldString)
+	subdir := filepath.Join(tempDir, "bar", "baz", "stuff")
+	check(os.MkdirAll(subdir, 0755))
+	writeTestFile(subdir, "foo", helloWorldString)
+
+	expectedChecksums := map[string]string{
+		"bar/baz/stuff/foo": helloWorldChecksum,
+		"foo":               helloWorldChecksum,
+	}
+
+	expectedCreationTime := time.Now()
+
+	return expectedChecksums, expectedCreationTime
+}
+
+func TestFileChecksum(t *testing.T) {
+	tempDir, err := ioutil.TempDir("", "checksum")
+	check(err)
+
+	defer os.RemoveAll(tempDir)
+
+	testFile := writeTestFile(tempDir, "foo", helloWorldString)
+	fileChecksum := FileChecksum(testFile)
+	var fi os.FileInfo
+	fi, err = os.Stat(testFile)
+
+	correctModTime := fi.ModTime()
+	if fileChecksum.Checksum != helloWorldChecksum {
+		t.Fatalf("expected checksum %s; got %s", helloWorldChecksum, fileChecksum.Checksum)
+	}
+	if fileChecksum.ModTime != correctModTime {
+		t.Fatalf("expected modTime %s; got %s", correctModTime, fileChecksum.ModTime)
+	}
+}
+
+func TestDirectoryManifest(t *testing.T) {
+	tempDir, err := ioutil.TempDir("", "checksum")
+	check(err)
+
+	defer os.RemoveAll(tempDir)
+
+	expectedChecksums, expectedCreationTime := populateTestDirectory(tempDir)
+
+	config := Config{}
+	manifest := GenerateDirectoryManifest(tempDir, &config)
+
+	if manifest.Path != tempDir {
+		t.Fatalf("expected manifest path %s, got %s", tempDir, manifest.Path)
+	}
+
+	if math.Abs(float64(manifest.CreatedAt.Unix()-expectedCreationTime.Unix())) > 0 {
+		t.Fatalf("expected manifest createdAt within 5s of %v, got %v", expectedCreationTime, manifest.CreatedAt)
+	}
+
+	if len(manifest.Entries) != len(expectedChecksums) {
+		t.Fatalf(
+			"unexpected number of checksums! expected %d, got %d (%v)",
+			len(expectedChecksums),
+			len(manifest.Entries),
+			manifest.Entries,
+		)
+	}
+
+	for path, fileChecksum := range manifest.Entries {
+		if fileChecksum.Checksum != expectedChecksums[path] {
+			t.Fatalf("checksum mismatch; expected %s, got %s", expectedChecksums[path], fileChecksum.Checksum)
+		}
+	}
+}
+
+func TestManifestExclusionOnFile(t *testing.T) {
+	tempDir, err := ioutil.TempDir("", "checksum")
+	check(err)
+
+	defer os.RemoveAll(tempDir)
+
+	populateTestDirectory(tempDir)
+
+	config := Config{
+		ExcludedFiles: []string{"foo"},
+	}
+
+	manifest := GenerateDirectoryManifest(tempDir, &config)
+
+	if !reflect.DeepEqual(manifest.Entries, map[string]ChecksumRecord{}) {
+		t.Fatalf("Entries mismatch; expected %v, got %v", map[string]ChecksumRecord{}, manifest.Entries)
+	}
+}
+
+func TestManifestExclusionOnFolder(t *testing.T) {
+	tempDir, err := ioutil.TempDir("", "checksum")
+	check(err)
+
+	defer os.RemoveAll(tempDir)
+
+	populateTestDirectory(tempDir)
+
+	config := Config{
+		ExcludedFiles: []string{"baz"},
+	}
+
+	manifest := GenerateDirectoryManifest(tempDir, &config)
+
+	entryPaths := []string{}
+	for path := range manifest.Entries {
+		entryPaths = append(entryPaths, path)
+	}
+	expectedEntryPaths := []string{"foo"}
+
+	if !reflect.DeepEqual(entryPaths, expectedEntryPaths) {
+		t.Fatalf("Entries mismatch; expected %v, got %v", expectedEntryPaths, entryPaths)
+	}
+}
+
+func TestManifestJSON(t *testing.T) {
+	tempDir, err := ioutil.TempDir("", "checksum")
+	check(err)
+
+	defer os.RemoveAll(tempDir)
+
+	expectedChecksums, expectedCreationTime := populateTestDirectory(tempDir)
+
+	config := Config{}
+	manifest := GenerateDirectoryManifest(tempDir, &config)
+
+	jsonBytes, err := json.Marshal(manifest)
+
+	var recreatedManifest DirectoryManifest
+	err = json.Unmarshal(jsonBytes, &recreatedManifest)
+	check(err)
+
+	if recreatedManifest.Path != tempDir {
+		t.Fatalf("expected JSON path %s, got %s", tempDir, recreatedManifest.Path)
+	}
+
+	if math.Abs(float64(recreatedManifest.CreatedAt.Unix()-expectedCreationTime.Unix())) > 0 {
+		t.Fatalf("expected manifest created_at within 0s of %v, got %v", expectedCreationTime, recreatedManifest.CreatedAt)
+	}
+
+	entries := recreatedManifest.Entries
+	if len(entries) != len(expectedChecksums) {
+		t.Fatalf(
+			"unexpected number of checksums! expected %d, got %d (%v)",
+			len(expectedChecksums),
+			len(entries),
+			entries,
+		)
+	}
+
+	for path, fileChecksum := range entries {
+		if fileChecksum.Checksum != expectedChecksums[path] {
+			t.Fatalf("checksum mismatch; expected %s, got %s", expectedChecksums[path], fileChecksum.Checksum)
+		}
+	}
+}
+
+func TestManifestComparison(t *testing.T) {
+	newCreatedAt := time.Now()
+	newModTime := time.Now()
+	oldCreatedAt := newCreatedAt.Add(-24 * time.Hour)
+	oldModTime := newModTime.Add(-24 * time.Hour)
+	oldManifest := DirectoryManifest{
+		Path:      "/old/stuff",
+		CreatedAt: oldCreatedAt,
+		Entries: map[string]ChecksumRecord{
+			"silently_corrupted": ChecksumRecord{Checksum: "asdf", ModTime: oldModTime},
+			"not_changed":        ChecksumRecord{Checksum: "zxcv", ModTime: oldModTime},
+			"modified":           ChecksumRecord{Checksum: "qwer", ModTime: oldModTime},
+			"touched":            ChecksumRecord{Checksum: "olkm", ModTime: oldModTime},
+			"deleted":            ChecksumRecord{Checksum: "jklh", ModTime: oldModTime},
+		},
+	}
+
+	newManifest := DirectoryManifest{
+		Path:      "/new/thing",
+		CreatedAt: newCreatedAt,
+		Entries: map[string]ChecksumRecord{
+			"silently_corrupted": ChecksumRecord{Checksum: "zzzz", ModTime: oldModTime},
+			"not_changed":        ChecksumRecord{Checksum: "zxcv", ModTime: oldModTime},
+			"modified":           ChecksumRecord{Checksum: "tyui", ModTime: newModTime},
+			"touched":            ChecksumRecord{Checksum: "olkm", ModTime: newModTime},
+			"added":              ChecksumRecord{Checksum: "bnmv", ModTime: newModTime},
+		},
+	}
+
+	comparison := CompareManifests(oldManifest, newManifest)
+
+	expectedDeletedPaths := []string{"deleted"}
+	if !reflect.DeepEqual(comparison.DeletedPaths, expectedDeletedPaths) {
+		t.Fatalf("expected DeletedPaths %v; got %v", expectedDeletedPaths, comparison.DeletedPaths)
+	}
+
+	expectedModifiedPaths := []string{"modified"}
+	if !reflect.DeepEqual(comparison.ModifiedPaths, expectedModifiedPaths) {
+		t.Fatalf("expected ModifiedPaths %v; got %v", expectedModifiedPaths, comparison.ModifiedPaths)
+	}
+
+	expectedFlaggedPaths := []string{"silently_corrupted"}
+	if !reflect.DeepEqual(comparison.FlaggedPaths, expectedFlaggedPaths) {
+		t.Fatalf("expected FlaggedPaths %v; got %v", expectedFlaggedPaths, comparison.FlaggedPaths)
+	}
+
+	expectedAddedPaths := []string{"added"}
+	if !reflect.DeepEqual(comparison.AddedPaths, expectedAddedPaths) {
+		t.Fatalf("expected AddedPaths %v; got %v", expectedAddedPaths, comparison.AddedPaths)
+	}
+}
