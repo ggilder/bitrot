7761e4430e65f417a39969463d33b1d3a4092dfe
Gabriel Gilder <gabriel@gabrielgilder.com>
2019-12-05 00:36:49 -0800

    Add summary section to report

--- comparison_report.go
+++ comparison_report.go
@@ -15,6 +15,31 @@ func NewComparisonReport(comparison *ManifestComparison) *ComparisonReport {
 }
 
 func (report *ComparisonReport) ReportString() string {
+	return report.SummaryString() + "\n\n\n" + report.DetailString()
+}
+
+func (report *ComparisonReport) SummaryString() string {
+	s := ""
+	if report.mc.Success() {
+		s += "SUCCESS"
+	} else {
+		s += "FAILURE"
+	}
+	s += "\n\n"
+
+	s += fmt.Sprintf("%d files compared.\n\n", report.mc.TotalChecked())
+
+	s += report.summaryLine("Unchanged", report.mc.UnchangedPaths)
+	s += report.summaryLine("Added", report.mc.AddedPaths)
+	s += report.summaryLine("Deleted", report.mc.DeletedPaths)
+	s += fmt.Sprintf("Renamed paths: %d\n", len(report.mc.RenamedPaths))
+	s += report.summaryLine("Modified", report.mc.ModifiedPaths)
+	s += report.summaryLine("Flagged", report.mc.FlaggedPaths)
+
+	return s
+}
+
+func (report *ComparisonReport) DetailString() string {
 	return report.unchangedSection() +
 		report.pathSection("Added", report.mc.AddedPaths) +
 		report.pathSection("Deleted", report.mc.DeletedPaths) +
@@ -23,10 +48,13 @@ func (report *ComparisonReport) ReportString() string {
 		report.pathSection("Flagged", report.mc.FlaggedPaths)
 }
 
-func (report *ComparisonReport) pathSection(description string, paths []string) string {
-	s := ""
+func (report *ComparisonReport) summaryLine(description string, paths []string) string {
 	count := len(paths)
-	s += fmt.Sprintf("%s paths: %d\n", description, count)
+	return fmt.Sprintf("%s paths: %d\n", description, count)
+}
+
+func (report *ComparisonReport) pathSection(description string, paths []string) string {
+	s := report.summaryLine(description, paths)
 	for _, path := range paths {
 		s += fmt.Sprintf("    %s\n", path)
 	}
@@ -34,8 +62,7 @@ func (report *ComparisonReport) pathSection(description string, paths []string) 
 }
 
 func (report *ComparisonReport) unchangedSection() string {
-	count := len(report.mc.UnchangedPaths)
-	return fmt.Sprintf("Unchanged paths: %d\n", count)
+	return report.summaryLine("Unchanged", report.mc.UnchangedPaths)
 }
 
 func (report *ComparisonReport) renamedSection() string {
